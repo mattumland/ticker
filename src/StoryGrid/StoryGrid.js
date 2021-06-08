7851ec63c5781267d2fdd9945b0fd8a564bd58da
Matt Umland <mattumland@gmail.com>
2021-06-08 14:22:40 -0400

    Pass props and render stories without styling

--- src/StoryGrid/StoryGrid.js
+++ src/StoryGrid/StoryGrid.js
@@ -3,13 +3,18 @@ import React from 'react';
 
 const StoryGrid = (props) => {
     const storyList = props.stories.map(story => {
-        <button className='story-card' id={story.id}>
+        return (
+        <button className='story-card' id={story.id} key={story.id}>
             <img src={story.imageLink} alt={story.caption} />
             <div className='headline-box'>
                 <h3>{story.headline}</h3>
             </div>
         </button>
+        )
     }) 
+
+    console.log(storyList)
+
 
     return (
         <section className='story-grid'>
