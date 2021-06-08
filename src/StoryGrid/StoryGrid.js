6823569927b56bf1de802c38ea86f647b91490b3
Matt Umland <mattumland@gmail.com>
2021-06-08 14:18:52 -0400

    Add props with story data

--- src/StoryGrid/StoryGrid.js
+++ src/StoryGrid/StoryGrid.js
@@ -1,21 +1,19 @@
 import './StoryGrid.css'
 import React from 'react';
 
-const StoryGrid = (stories) => {
-
+const StoryGrid = (props) => {
     const storyList = props.stories.map(story => {
-        <button id={story.id}>
+        <button className='story-card' id={story.id}>
             <img src={story.imageLink} alt={story.caption} />
             <div className='headline-box'>
                 <h3>{story.headline}</h3>
             </div>
         </button>
-
     }) 
 
     return (
         <section className='story-grid'>
-
+            { storyList.length ? storyList : <h4>No stories</h4>}
         </section>
     )
 }
