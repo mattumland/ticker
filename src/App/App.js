bf8f4754ff6e60dc67cd6e605eb2db3f3980800e
Matt Umland <mattumland@gmail.com>
2021-06-08 11:36:51 -0400

    Add initial state to App

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,19 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 
 function App() {
+
+  const [error, setError] = useState('')
+  const [allStories, setAllStories] = useState([])
+  const [storyList, setStoryList] = useState([])
+  const [filter, setFilter] = useState('')
+  const [storyDetails, setStoryDetails] = useState({})
+
+  useEffect(() => {
+
+  }, [])
+
   return (
     <main>
 
