8f75e8f6343e3adf9686e282cbc98069f119e81c
Matt Umland <mattumland@gmail.com>
2021-06-08 12:38:34 -0400

    Build header boiler plate with content, no behavior

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,15 +1,30 @@
 import './Header.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 const Header = () => {
+
+    const [currentFilter, updateCurrentFilter] = ('')
+
+
     return (
         <header>
-            <section>
+            <section className='title-block'>
                 <h1>TICKER</h1>
-                <h2>this stories you care about</h2>
+                <h2>the stories you care about</h2>
             </section>
-            <section>
-                <button></button>
+            <section className='button-container'>
+                <div className='small-button-container'>
+                    <button>ALL STORIES</button>
+                    <button>HEALTH</button>
+                    <button>US</button>
+                    <button>WORLD</button>
+                </div>
+                <div className='small-button-container'>
+                    <button>TECH</button>
+                    <button>BUSINESS</button>
+                    <button>OPINION</button>
+                    <button>TRAVEL</button>
+                </div>
             </section>
         </header>
     )
