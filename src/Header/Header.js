b12b51ac145fede60da6c48594717dff68ec8ba6
Matt Umland <mattumland@gmail.com>
2021-06-08 18:56:30 -0400

    Add ternary to apply active class based on filter prop

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,7 +1,7 @@
 import './Header.css';
 import React from 'react';
 
-const Header = ({ filter, updateFilter }) => {
+const Header = ({ currentFilter, updateFilter }) => {
     
     return (
         <header>
@@ -11,16 +11,16 @@ const Header = ({ filter, updateFilter }) => {
             </section>
             <section className='button-container'>
                 <div className='small-button-container'>
-                    <button id='' onClick={e => updateFilter(e)}>ALL STORIES</button>
-                    <button id='health' onClick={e => updateFilter(e)}>HEALTH</button>
-                    <button id='us' onClick={e => updateFilter(e)}>US</button>
-                    <button id='world' onClick={e => updateFilter(e)}>WORLD</button>
+                    <button className={(currentFilter === '') ? 'active' : ''} id='' onClick={e => updateFilter(e)}>ALL STORIES</button>
+                    <button className={(currentFilter === 'health') ? 'active' : ''}  id='health' onClick={e => updateFilter(e)}>HEALTH</button>
+                    <button className={(currentFilter === 'us') ? 'active' : ''} id='us' onClick={e => updateFilter(e)}>US</button>
+                    <button className={(currentFilter === 'world') ? 'active' : ''} id='world' onClick={e => updateFilter(e)}>WORLD</button>
                 </div>
                 <div className='small-button-container'>
-                    <button id='sci' onClick={e => updateFilter(e)}>SCIENCE</button>
-                    <button id='business' onClick={e => updateFilter(e)}>BUSINESS</button>
-                    <button id='opinion' onClick={e => updateFilter(e)}>OPINION</button>
-                    <button id='arts' onClick={e => updateFilter(e)}>ARTS</button>
+                    <button className={(currentFilter === 'sci') ? 'active' : ''} id='sci' onClick={e => updateFilter(e)}>SCIENCE</button>
+                    <button className={(currentFilter === 'business') ? 'active' : ''} id='business' onClick={e => updateFilter(e)}>BUSINESS</button>
+                    <button className={(currentFilter === 'opinion') ? 'active' : ''} id='opinion' onClick={e => updateFilter(e)}>OPINION</button>
+                    <button className={(currentFilter === 'arts') ? 'active' : ''} id='arts' onClick={e => updateFilter(e)}>ARTS</button>
                 </div>
             </section>
         </header>
