5244ba492a61af8c9cccf5b6a980a230aeee2e85
Matt Umland <mattumland@gmail.com>
2021-06-08 12:12:05 -0400

    Create header component and stylesheet

---
+++ src/Header/Header.js
@@ -0,0 +1,18 @@
+import './Header.css';
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <section>
+                <h1>TICKER</h1>
+                <h2>this stories you care about</h2>
+            </section>
+            <section>
+                <button></button>
+            </section>
+        </header>
+    )
+}
+
+export default Header
