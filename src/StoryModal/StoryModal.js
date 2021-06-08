22d2e8fa29b7f9ba1423fcf27d08a5bc76634020
Matt Umland <mattumland@gmail.com>
2021-06-08 19:50:25 -0400

    Add StoryModal component with boilerplate and stylesheet

---
+++ src/StoryModal/StoryModal.js
@@ -0,0 +1,13 @@
+import './StoryModal.css'
+import React from 'react'
+
+const StoryModal = ({ story }) => {
+
+    return (
+        <section>
+
+        </section>
+    )
+}
+
+export default StoryModal
