057067715901841e3a77b6723469528632590ad0
Matt Umland <mattumland@gmail.com>
2021-06-09 10:25:00 -0400

    Add content to story modal

--- src/StoryModal/StoryModal.js
+++ src/StoryModal/StoryModal.js
@@ -4,9 +4,28 @@ import React from 'react'
 const StoryModal = ({ story }) => {
 
     return (
-        <section>
-
-        </section>
+        <div className='modal'>
+            <section className='story-container'>
+                <aside className='title-box'>
+                    <h3 className='title'>{story.title}</h3>
+                    <button className='xbutton'>X</button>
+                </aside>
+                <div className='full-img-box'>
+                    <img className='full-img' src={story.imageLink} alt={story.caption} />
+                </div>
+                <aside className='details-box'>
+                    <h4>{story.byline}</h4>
+                    <h5>{story.publishedDate}</h5>
+                    <p>{story.abstract}</p>
+                    <h5>Image Caption</h5>
+                    <p>{story.imageCaption}</p>
+                    <h5>Image credit</h5>
+                    <p>{story.imageCredit}</p>
+                    <h5>Click here for the full story</h5>
+                    <a>{story.link}</a>
+                </aside>
+            </section>
+        </div>
     )
 }
 
